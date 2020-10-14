1dd84271aec974dc8eb9955e7215275f639edb6a
Marco <marco.pampaloni1@gmail.com>
2020-10-14 16:31:29 +0200

    Update dataset generation script

--- dataset-generation/generate_dataset.sh
+++ dataset-generation/generate_dataset.sh
@@ -43,7 +43,6 @@ echo "setoption Record" >& ${COPROC[1]} # Tell the engine to record the evaluati
 LINES=$(wc -l < $POSITIONS)
 COUNT=1
 
-<<<<<<< HEAD
 function restart() {
   wait $COPROC_PID;
   echo "Restoring failed coprocess...";
@@ -51,51 +50,25 @@ function restart() {
   echo "setoption Record" >& ${COPROC[1]};
 }
 
-=======
->>>>>>> ae02729548ab2c689097885f565ae3828c374ef2
-
 while IFS= read -r fen; do
   # print a progress bar if this is the main thread
   if [[ $MAIN_THREAD == true ]]; then
-<<<<<<< HEAD
-    # progress $((100 * COUNT / LINES))
-    echo
-  fi
-  echo "EVALUATING $fen"
-=======
     progress $((100 * COUNT / LINES))
   fi
   # echo "EVALUATING $fen"
->>>>>>> ae02729548ab2c689097885f565ae3828c374ef2
   echo "position fen $fen" >& ${COPROC[1]}
   echo "go depth $DEPTH" >& ${COPROC[1]}
 
   { 
-<<<<<<< HEAD
   while IFS= read -u ${COPROC[0]} -r line || restart; do
-=======
-  while IFS= read -t 0.5 -u ${COPROC[0]} -r line; do
->>>>>>> ae02729548ab2c689097885f565ae3828c374ef2
     if [[ $line == bestmove* ]]; then # Done searching
       break
     fi
     if [[ $line == Position* ]]; then # Error in the position
-<<<<<<< HEAD
       restart
       break
     fi
   done } || { restart; } 
-=======
-      wait $COPROC_PID
-      coproc $ENGINE
-      echo "setoption Record" >& ${COPROC[1]} # Tell the engine to record the evaluations in a csv file
-      break
-    fi
-  done } || { wait $COPROC_PID;
-              echo "Restoring failed coprocess...";
-              coproc $ENGINE;
-              echo "setoption Record" >& ${COPROC[1]}; } 
->>>>>>> ae02729548ab2c689097885f565ae3828c374ef2
   COUNT=$((COUNT+1))
 done < $POSITIONS
 
